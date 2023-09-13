ce451aecd76416ffc0f9ad3e19732fe35408bfde
Odion Akhes <akhetuamhenodion@gmail.com>
2023-09-13 10:47:47 +0100

    set up routes

---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+import Sidebar from '../components/Sidebar';
+const SharedLayout = () => {
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
+}
+
+export default SharedLayout
